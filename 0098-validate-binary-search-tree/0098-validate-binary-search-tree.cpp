d77e9e7b655e002307f8d3fc85674c788be3e0c9
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-02 07:51:24 +0530

    Time: 11 ms (36.05%), Space: 20.5 MB (10.74%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,18 +11,16 @@
  */
 class Solution {
 public:
-    void inorder_fun(TreeNode* root, vector<int> &ans){
-        if(root == NULL) return;
-        inorder_fun(root->left, ans);
-        ans.push_back(root->val);
-        inorder_fun(root->right, ans);
+    bool solve(TreeNode* root, long long int lowerBound, long long int upperBound){
+        if(root == NULL) return true;
+        return ((root->val > lowerBound) 
+        && (root->val < upperBound) 
+        && solve(root->left, lowerBound, root->val) 
+        && solve(root->right, root->val, upperBound));
     }
     bool isValidBST(TreeNode* root) {
-        vector<int> inorder;
-        inorder_fun(root, inorder);
-        for(int i = 1 ; i < inorder.size() ; i++){
-            if(inorder[i] <= inorder[i - 1]) return false;
-        }
-        return true;
+        long long int lowerBound = -2147483658;
+        long long int upperBound = 2147483657;
+        return solve(root, lowerBound, upperBound);
     }
 };
