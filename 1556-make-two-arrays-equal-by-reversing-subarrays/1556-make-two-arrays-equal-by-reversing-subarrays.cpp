7958594d30531dc03e96dbebe79218c512685407
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-03 06:46:12 +0530

    Time: 10 ms (52.84%), Space: 21 MB (6.79%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        int n = target.size();
-        // int i = 0, start = 0, end = 0;
-        // while(i < n && end < n){
-        //     if(target[i] != arr[end]) end++;
-        //     else{
-        //         if(end - start > 0){
-        //             reverse(arr.begin() + start, arr.begin() + end + 1);
-        //             start++;
-        //             i++;
-        //         }
-        //         else{
-        //             i++;
-        //             start++;
-        //             end++;
-        //         }
-        //     }
-        // }
-        // return target == arr;
         unordered_map<int, int> mp1;
         unordered_map<int, int> mp2;
         for(int i : target){
@@ -27,13 +9,10 @@ public:
         for(int i : arr){
             mp2[i]++;
         }
-        bool ans = true;
-        for(auto it : mp1){
-            if(mp2.find(it.first) == mp2.end()) ans = false;
-            else{
-                if(mp2.find(it.first)->second != it.second) ans = false;
-            }
+        if(mp1.size() != mp2.size()) return false;
+        for(auto it : mp2){
+            if(mp1[it.first] != it.second) return false;
         }
-        return ans;
+        return true;
     }
 };
