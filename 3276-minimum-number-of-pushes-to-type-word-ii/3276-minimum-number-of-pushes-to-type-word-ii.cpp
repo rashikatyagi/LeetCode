96042348b5b350e3b9e352be912cea143d844222
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-06 06:23:44 +0530

    Time: 67 ms (83.57%), Space: 24.8 MB (64.48%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    
     int minimumPushes(string word) {
-        unordered_map<char, int> mp;
-        for(char s : word){
-            mp[s]++;
+        vector<int> arr(26, 0);
+        for(char &ch : word){
+            arr[ch - 'a']++;
         }
-        vector<pair<char, int> > arr(mp.begin(), mp.end());
-        auto comp = [](const pair<char, int> &a, const pair<char, int> &b){
-        return a.second > b.second;
-    };
-        sort(arr.begin(), arr.end(), comp);
+        sort(arr.begin(), arr.end(), [](const int &a, const int &b){
+        return a > b;
+    });
         
         int count = 1;
         int ans = 0;
-        for(int i = 0 ; i < arr.size() ; i++){
-            ans += count * arr[i].second;
+        for(int i = 0 ; i < 26 ; i++){
+            ans += count * arr[i];
             if((i + 1) % 8 == 0) count++;
         }
         return ans;
     }
+    
 };
