ffe645f7d20d4359c667222bfcb12aefa0b10f05
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-08 17:00:12 +0530

    Time: 14 ms (23.17%), Space: 13.7 MB (48.36%) - LeetHub

---
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int sr, int sc) {
+        vector<vector<int>> ans;
+        int it = 1;
+        int count = 1;
+        int total = rows * cols;
+        ans.push_back({sr, sc});
+        while(count < total){
+            for(int i = 1 ; count < total && i <= it ; i++){
+                sc++;
+                if(sr >= 0 && sr < rows && sc >= 0 && sc < cols){
+                    ans.push_back({sr, sc});
+                    count++;
+                }
+            }
+            for(int i = 1 ; count < total && i <= it ; i++){
+                sr++;
+                if(sr >= 0 && sr < rows && sc >= 0 && sc < cols){
+                    ans.push_back({sr, sc});
+                    count++;
+                }
+            }
+            it++;
+            for(int i = 1 ; count < total && i <= it ; i++){
+                sc--;
+                if(sr >= 0 && sr < rows && sc >= 0 && sc < cols){
+                    ans.push_back({sr, sc});
+                    count++;
+                }
+            }
+            for(int i = 1; count < total && i <= it ; i++){
+                sr--;
+                if(sr >= 0 && sr < rows && sc >= 0 && sc < cols){
+                    ans.push_back({sr, sc});
+                    count++;
+                }
+            }
+            it++;
+        }
+        return ans;
+    }
+};
