6a5d20aeb749a4845fa3367775ad1736469e6d0a
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-09 08:18:16 +0530

    Time: 0 ms (100%), Space: 7.7 MB (38.46%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(t == "" && s == "") return true;
+        int j = 0;
+        for(int i = 0 ; i < t.length() ; i++){
+            if(s[j] == t[i]) j++;
+            if(j == s.length()) return true;
+        }
+        return false;
+    }
+};
