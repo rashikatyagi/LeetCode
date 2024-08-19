c7114c52efd1ea67496425444585a7be6ff8e254
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-19 13:05:02 +0530

    Time: 4 ms (32.88%), Space: 8.6 MB (49.09%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -10,8 +10,8 @@ public:
 
 class compare{
 public:
-    bool operator()(Info* a, Info* b){
-        return a->count < b->count;
+    bool operator()(Info a, Info b){
+        return a.count < b.count;
     }
 };
 
@@ -23,27 +23,27 @@ public:
             frequency[ch - 'a']++;
         }
         //creating the max heap
-        priority_queue<Info*, vector<Info*>, compare> pq;
+        priority_queue<Info, vector<Info>, compare> pq;
         //filling the max heap
         for(int i = 0 ; i < 26 ; i++){
             if(frequency[i] > 0){
-                Info* temp = new Info(i + 'a', frequency[i]);
+                Info temp(i + 'a', frequency[i]);
                 pq.push(temp);
             }
         }
         int i = 0;
         while(i < s.length()){
-            if(pq.size() == 1 && pq.top()->count > 1) return "";
-            Info* temp1 = pq.top();
+            if(pq.size() == 1 && pq.top().count > 1) return "";
+            Info temp1 = pq.top();
             pq.pop();
-            s[i++] = temp1->val;
-            temp1->count--;
-            Info* temp2 = pq.top();
+            s[i++] = temp1.val;
+            temp1.count--;
+            Info temp2 = pq.top();
             pq.pop();
-            s[i++] = temp2->val;
-            temp2->count--;
-            if(temp1->count > 0) pq.push(temp1);
-            if(temp2->count > 0) pq.push(temp2);
+            s[i++] = temp2.val;
+            temp2.count--;
+            if(temp1.count > 0) pq.push(temp1);
+            if(temp2.count > 0) pq.push(temp2);
         }
         return s;
     }
