6411626eb028696a283f341bb02f23d48c9dc08e
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-24 05:55:50 +0530

    Time: 3 ms (66.63%), Space: 7.4 MB (57.77%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int fib_topDown(int n, vector<int> &dp){
-        if(n == 0 || n == 1) return n;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = fib_topDown(n - 1, dp) + fib_topDown(n - 2, dp);
-        return dp[n];
-    }
+    // Using Tabulation method
     int fib(int n) {
+        // STEP 1 make dp array
         vector<int> dp(n + 1, -1);
-        return fib_topDown(n, dp);
+        
+        // STEP 2 enter manually some inital data using base cases of recursion
+        dp[0] = 0;
+        if(n == 0) return 0;
+        //if n was zero then an array of 1 size would be made which will give segmentation fault at dp[1]
+        dp[1] = 1;
+
+        // STEP 3 fill the dp array using logic of recursive call of recursion
+        for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
