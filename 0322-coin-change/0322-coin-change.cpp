057d8eca6d7aa9df701e1a272edbca1281469588
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-27 15:01:21 +0530

    Time: 16 ms (94.66%), Space: 16.2 MB (83.63%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount){
-        if(amount == 0 ) return 0;
-        if(amount < 0) return INT_MAX;
-        int mini = INT_MAX;
+    // int solve(vector<int>& coins, int amount){
+    //     if(amount == 0 ) return 0;
+    //     if(amount < 0) return INT_MAX;
+    //     int mini = INT_MAX;
 
-        for(int i = 0 ; i < coins.size() ; i++){
-            if(coins[i] <= amount){
-                int recAns = solve(coins, amount - coins[i]);
-                if(recAns != INT_MAX) recAns += 1;
-                mini = min(recAns, mini);
-            }
-        }
-        return mini;
-    }
+    //     for(int i = 0 ; i < coins.size() ; i++){
+    //         if(coins[i] <= amount){
+    //             int recAns = solve(coins, amount - coins[i]);
+    //             if(recAns != INT_MAX) recAns += 1;
+    //             mini = min(recAns, mini);
+    //         }
+    //     }
+    //     return mini;
+    // }
     int solveMemo(vector<int>& coins, int amount, vector<int>&dp){
         if(amount == 0 ) return 0;
         if(amount < 0) return INT_MAX;
@@ -33,9 +33,24 @@ public:
         dp[amount] = mini;
         return mini;
     }
-    int coinChange(vector<int>& coins, int amount) {
+    int solveTab(vector<int>& coins, int amount){
         vector<int> dp(amount + 1, -1);
-        int ans = solveMemo(coins, amount, dp);
+        dp[0] = 0;
+        for(int val = 1 ; val <= amount ; val++){
+            int mini = INT_MAX;
+            for(int i = 0 ; i < coins.size() ; i++){
+                if(coins[i] <= val){
+                    int recAns = dp[val - coins[i]];
+                    if(recAns != INT_MAX) recAns += 1;
+                    mini = min(recAns, mini);
+                }
+            }
+            dp[val] = mini;
+        }
+        return dp[amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int ans = solveTab(coins, amount);
         if(ans == INT_MAX) return -1;
         return ans;
     }
