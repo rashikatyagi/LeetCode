30e92d9494cfb4abe31b872384019418baf38da9
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-28 08:23:03 +0530

    Time: 90 ms (96.24%), Space: 49.5 MB (98.25%) - LeetHub

--- 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,23 +1,19 @@
-class compare{
-public:
-    bool operator()(pair<int, int> a, pair<int, int> b){
-        return a.second > b.second;
-    }
-};
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        unordered_map<int, int> mp;
-        for(int &it : arr){
-            mp[it]++;
+        sort(arr.begin(), arr.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int count = 1;
+        for(int i = 1 ; i < arr.size() ; i++){
+            if(arr[i] == arr[i - 1]) count++;
+            else{
+                pq.push(count);
+                count = 1;
+            }
         }
-        if(k == 0) return mp.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
-        for(auto &it : mp){
-            pq.push(make_pair(it.first, it.second));
-        }
-        while(k >= pq.top().second){
-            k -= pq.top().second;
+        pq.push(count);
+        while(k >= pq.top()){
+            k -= pq.top();
             pq.pop();
         }
         return pq.size();
