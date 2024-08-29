34927445b5f9cd1b46b029b0df1367770901173e
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-29 07:24:54 +0530

    Time: 7 ms (16.35%), Space: 8.9 MB (78.22%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int index = 0;
+        int n = s.length();
+        bool isNegative = false;
+
+        // remove leading whitespaces
+        while(index < n && s[index] == ' ') index++;
+
+        //checking for sign
+        if(index < n && s[index] == '-' || s[index] == '+'){
+            isNegative = (s[index] == '-') ? true : false;
+            index++;
+        }
+
+        //create the number
+        int number = 0;
+        while((index) < n && s[index] >= '0' && s[index] <= '9'){
+            int digit = s[index] - '0';
+            if(number > INT_MAX / 10 || (number == INT_MAX / 10 && digit > 7)){
+                return (isNegative) ? INT_MIN : INT_MAX;
+            }
+            number = number * 10 + digit;
+            index++;
+        }
+        if(isNegative) return -1 * number;
+        return number;
+    }
+};
