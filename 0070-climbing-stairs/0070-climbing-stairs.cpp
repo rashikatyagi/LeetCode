c2bac3a9f17673796e60bde1122e399cfd3e9bdf
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-31 10:53:17 +0530

    Time: 3 ms (29.68%), Space: 7.5 MB (59.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,16 +4,25 @@ public:
     //     if(n == 1 || n == 2) return n;
     //     return climbStairsRE(n - 1) + climbStairsRE(n - 2);
     // }
-    int climbStairsMem(int n, vector<int> &dp) {
+    // int climbStairsMem(int n, vector<int> &dp) {
+    //     if(n == 1 || n == 2) return n;
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //     dp[n] = climbStairsMem(n - 1, dp) + climbStairsMem(n - 2, dp);
+    //     return dp[n];
+    // }
+    int climbStairsTab(int n) {
         if(n == 1 || n == 2) return n;
-        if(dp[n] != -1){
-            return dp[n];
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3 ; i <= n ; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        dp[n] = climbStairsMem(n - 1, dp) + climbStairsMem(n - 2, dp);
         return dp[n];
     }
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return climbStairsMem(n, dp);
+        return climbStairsTab(n);
     }
 };
