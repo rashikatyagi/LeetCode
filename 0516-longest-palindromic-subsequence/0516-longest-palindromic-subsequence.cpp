96d20898bca16d1f9b9916747b65e65814c4d9c4
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-08-31 09:31:28 +0530

    Time: 37 ms (91.01%), Space: 8.1 MB (94.68%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -41,26 +41,46 @@ public:
     //     dp[start][end] = ans;
     //     return ans;
     // }
-    int longestPalindromeSubseqTab(string s){
+    // int longestPalindromeSubseqTab(string s){
+    //     int n = s.length();
+    //     vector<vector<int> > dp(n, vector<int>(n, 0));
+    //     // base case manual updation
+    //     for(int i = 0 ; i < n ; i++){
+    //         dp[i][i] = 1;
+    //     }
+    //     for(int j = 1 ; j < n ; j++){
+    //         for(int i = j - 1 ; i >= 0 ; i--){
+    //             if(s[i] == s[j]){
+    //                 dp[i][j] = 2 + dp[i + 1][j - 1];
+    //             }
+    //             else{
+    //                 dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+    //             }
+    //         }
+    //     }
+    //     return dp[0][n - 1];
+    // }
+    int longestPalindromeSubseqTabSO(string s){
         int n = s.length();
-        vector<vector<int> > dp(n, vector<int>(n, 0));
-        // base case manual updation
-        for(int i = 0 ; i < n ; i++){
-            dp[i][i] = 1;
-        }
-        for(int j = 1 ; j < n ; j++){
-            for(int i = j - 1 ; i >= 0 ; i--){
-                if(s[i] == s[j]){
-                    dp[i][j] = 2 + dp[i + 1][j - 1];
+        vector<int> prev(n, 0);
+        vector<int> curr(n, 0);
+        
+        for(int j = 0 ; j < n ; j++){
+            for(int i = j ; i >= 0 ; i--){
+                if(i == j) curr[i] = 1;
+                else if(s[i] == s[j]){
+                    curr[i] = 2 + prev[i + 1];
                 }
                 else{
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                    curr[i] = max(curr[i + 1], prev[i]);
                 }
             }
+            //update
+            prev = curr;
         }
-        return dp[0][n - 1];
+        return curr[0];
     }
     int longestPalindromeSubseq(string s) {
-        return longestPalindromeSubseqTab(s);
+        return longestPalindromeSubseqTabSO(s);
     }
 };
