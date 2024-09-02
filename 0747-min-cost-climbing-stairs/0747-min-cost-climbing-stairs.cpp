d98809cb6e6d8761898e019386c3b60554f9e555
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-02 06:25:35 +0530

    Time: 7 ms (30.82%), Space: 16.9 MB (25%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -6,18 +6,36 @@ public:
     //     }
     //     return (cost[index] + min(solveRE(cost, index + 1), solveRE(cost, index + 2)));
     // }
-    int solveMem(vector<int>& cost, int index, vector<int> &dp){
-        if(index >= cost.size()){
-            return 0;
+    // int solveMem(vector<int>& cost, int index, vector<int> &dp){
+    //     if(index >= cost.size()){
+    //         return 0;
+    //     }
+    //     if(dp[index] != -1){
+    //         return dp[index];
+    //     }
+    //     return dp[index] = (cost[index] + min(solveMem(cost, index + 1, dp), solveMem(cost, index + 2, dp)));
+    // }
+    // int solveMem(vector<int>& cost, int index, vector<int> &dp){
+    //     if(index >= cost.size()){
+    //         return 0;
+    //     }
+    //     if(dp[index] != -1){
+    //         return dp[index];
+    //     }
+    //     return dp[index] = (cost[index] + min(solveMem(cost, index + 1, dp), solveMem(cost, index + 2, dp)));
+    // }
+    int solveTab(vector<int>& cost, int index){
+        int n = cost.size();
+        vector<int> dp(n, 0);
+        dp[n - 1] = cost[n - 1];
+        for(int i = n - 2 ; i >= 0 ; i--){
+            int temp = 0;
+            if(i + 2 < n) temp = dp[i + 2];
+            dp[i] = cost[i] + min(temp, dp[i + 1]);
         }
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        return dp[index] = (cost[index] + min(solveMem(cost, index + 1, dp), solveMem(cost, index + 2, dp)));
+        return dp[index];
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n, -1);
-        return min(solveMem(cost, 0, dp), solveMem(cost, 1, dp));
+        return min(solveTab(cost, 0), solveTab(cost, 1));
     }
 };
