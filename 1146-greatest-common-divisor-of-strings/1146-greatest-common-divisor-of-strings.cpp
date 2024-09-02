c4839345abaf9a790dcbbfc2c128fcbac662e2f2
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-02 12:38:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.35%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
-    int gcd(int a, int b){
-        while(a > 0 && b > 0){
-            if(a > b) a = a % b;
-            else b = b % a;
-        }
-        if(a == 0) return b;
-        return a;
-    }
     string gcdOfStrings(string str1, string str2) {
-        //check whether gcd exists or not
-        int n1 = str1.length(), n2 = str2.length();
-        if(str1 + str2 == str2 + str1){
-            //exists
-            return str1.substr(0, gcd(n1, n2));
-        }
-        return "";
+        return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(str1.length(), str2.length())) : "";
     }
 };
