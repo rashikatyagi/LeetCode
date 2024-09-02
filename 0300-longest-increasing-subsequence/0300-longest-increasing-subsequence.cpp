ece81bc3a5e2707bebfb1293ca5427041d401a82
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-02 08:44:30 +0530

    Time: 167 ms (43.24%), Space: 13.3 MB (47.36%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -32,23 +32,41 @@ public:
     //     int ans2 = solveMem(nums, currIndex + 1, prevIndex, dp);
     //     return dp[currIndex][prevIndex + 1] = max(ans1, ans2);
     // }
-    int solveTab(vector<int>& nums){
+    // int solveTab(vector<int>& nums){
+    //     int n = nums.size();
+    //     vector<vector<int> > dp(n + 1, vector<int>(n + 1, 0));
+
+    //     for(int currIndex = n - 1 ; currIndex >= 0 ; currIndex--){
+    //         for(int prevIndex = currIndex - 1 ; prevIndex >= -1 ; prevIndex--){
+    //             int ans1 = 0;
+    //             if(prevIndex == -1 || nums[currIndex] > nums[prevIndex]){
+    //                 ans1 = 1 + dp[currIndex + 1][currIndex + 1];
+    //             }
+    //             int ans2 = dp[currIndex + 1][prevIndex + 1];
+    //             dp[currIndex][prevIndex + 1] = max(ans1, ans2);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+    int solveTabSO(vector<int>& nums){
         int n = nums.size();
-        vector<vector<int> > dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> prev(n + 1, 0);
+        vector<int> curr(n + 1, 0);
 
         for(int currIndex = n - 1 ; currIndex >= 0 ; currIndex--){
             for(int prevIndex = currIndex - 1 ; prevIndex >= -1 ; prevIndex--){
                 int ans1 = 0;
                 if(prevIndex == -1 || nums[currIndex] > nums[prevIndex]){
-                    ans1 = 1 + dp[currIndex + 1][currIndex + 1];
+                    ans1 = 1 + prev[currIndex + 1];
                 }
-                int ans2 = dp[currIndex + 1][prevIndex + 1];
-                dp[currIndex][prevIndex + 1] = max(ans1, ans2);
+                int ans2 = prev[prevIndex + 1];
+                curr[prevIndex + 1] = max(ans1, ans2);
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return curr[0];
     }
     int lengthOfLIS(vector<int>& nums) {
-        return solveTab(nums);
+        return solveTabSO(nums);
     }
 };
