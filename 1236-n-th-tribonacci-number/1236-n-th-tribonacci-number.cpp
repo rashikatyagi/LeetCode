ae59858354c4cd593d131043597dc51b23b43ba7
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-02 05:42:17 +0530

    Time: 3 ms (22.83%), Space: 7.6 MB (14.26%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -5,19 +5,31 @@ public:
     //     if(n == 2) return 1;
     //     return solveRE(n - 1) + solveRE(n - 2) + solveRE(n - 3);
     // }
-    int solveMem(int n, vector<int>&dp){
-        if(n == 0 || n == 1) return n;
-        if(n == 2) return 1;
+    // int solveMem(int n, vector<int>&dp){
+    //     if(n == 0 || n == 1) return n;
+    //     if(n == 2) return 1;
 
-        if(dp[n] != -1){
-            return dp[n];
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+
+    //     dp[n] = solveMem(n - 1, dp) + solveMem(n - 2, dp) + solveMem(n - 3, dp);
+    //     return dp[n];
+    // }
+    int solveTab(int n){
+        if(n == 0) return 0;
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        if(n == 1) return dp[n];
+        dp[2] = 1;
+        if(n == 2) return dp[n];
+
+        for(int i = 3 ; i <= n ; i++){
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-
-        dp[n] = solveMem(n - 1, dp) + solveMem(n - 2, dp) + solveMem(n - 3, dp);
         return dp[n];
     }
     int tribonacci(int n) {
-        vector<int> dp(n + 1, -1);
-        return solveMem(n, dp);
+        return solveTab(n);
     }
 };
