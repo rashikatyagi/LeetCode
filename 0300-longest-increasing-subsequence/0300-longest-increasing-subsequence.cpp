c58932e8bc07191b5c6156cd2d67a3c923e3872b
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-02 08:19:48 +0530

    Time: 249 ms (34.42%), Space: 295 MB (20.4%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -13,28 +13,42 @@ public:
     //     int ans2 = solveRE(nums, currIndex + 1, prevIndex);
     //     return max(ans1, ans2);
     // }
-    int solveMem(vector<int>& nums, int currIndex, int prevIndex, vector<vector<int> > &dp){
-        if(currIndex >= nums.size()){
-            return 0;
-        }
+    // int solveMem(vector<int>& nums, int currIndex, int prevIndex, vector<vector<int> > &dp){
+    //     if(currIndex >= nums.size()){
+    //         return 0;
+    //     }
 
-        // check if already present in dp or not
-        if(dp[currIndex][prevIndex + 1] != -1){
-            return dp[currIndex][prevIndex + 1];
-        }
+    //     // check if already present in dp or not
+    //     if(dp[currIndex][prevIndex + 1] != -1){
+    //         return dp[currIndex][prevIndex + 1];
+    //     }
 
-        // case of inclusion
-        int ans1 = 0;
-        if(prevIndex == -1 || nums[currIndex] > nums[prevIndex]){
-            ans1 = 1 + solveMem(nums, currIndex + 1, currIndex, dp);
+    //     // case of inclusion
+    //     int ans1 = 0;
+    //     if(prevIndex == -1 || nums[currIndex] > nums[prevIndex]){
+    //         ans1 = 1 + solveMem(nums, currIndex + 1, currIndex, dp);
+    //     }
+    //     //case of exclusion
+    //     int ans2 = solveMem(nums, currIndex + 1, prevIndex, dp);
+    //     return dp[currIndex][prevIndex + 1] = max(ans1, ans2);
+    // }
+    int solveTab(vector<int>& nums){
+        int n = nums.size();
+        vector<vector<int> > dp(n + 1, vector<int>(n + 1, 0));
+
+        for(int currIndex = n - 1 ; currIndex >= 0 ; currIndex--){
+            for(int prevIndex = currIndex - 1 ; prevIndex >= -1 ; prevIndex--){
+                int ans1 = 0;
+                if(prevIndex == -1 || nums[currIndex] > nums[prevIndex]){
+                    ans1 = 1 + dp[currIndex + 1][currIndex + 1];
+                }
+                int ans2 = dp[currIndex + 1][prevIndex + 1];
+                dp[currIndex][prevIndex + 1] = max(ans1, ans2);
+            }
         }
-        //case of exclusion
-        int ans2 = solveMem(nums, currIndex + 1, prevIndex, dp);
-        return dp[currIndex][prevIndex + 1] = max(ans1, ans2);
+        return dp[0][0];
     }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int> > dp(n, vector<int>(n + 1, -1));
-        return solveMem(nums, 0, -1, dp);
+        return solveTab(nums);
     }
 };
