78a30ff55469f451f28d9cd68499d666ac35341a
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-03 05:43:35 +0530

    Time: 29 ms (51.75%), Space: 16.5 MB (63.56%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        string ans = "";
         for(int i = num.length() - 1 ; i >= 0 ; i--){
             if((num[i] - '0') & 1){
-                ans = num.substr(0, i + 1);
-                break;
+                return num.substr(0, i + 1);
             }
         }
-        return ans;
+        return "";
     }
 };
