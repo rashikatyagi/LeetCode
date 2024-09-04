4c8883cc6636aa10a750f5e5ee62ce8658bff2cd
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-04 13:53:03 +0530

    Time: 26 ms (21.98%), Space: 31.4 MB (14.85%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
+    bool check(vector<int> a, vector<int> b){
+        for(int i = 0 ; i < 26 ; i++){
+            if(a[i] != b[i]) return false;
+        }
+        return true;
+    }
     bool checkInclusion(string s1, string s2) {
-        sort(s1.begin(), s1.end());
+        vector<int> fre1(26, 0);
+        vector<int> fre2(26, 0);
         int n = s1.length();
         int m = s2.length();
-        for(int i = 0 ; i <= m - n ; i++){
-            string temp = s2.substr(i, n);
-            sort(temp.begin(), temp.end());
-            if(s1 == temp) return true;
+        if(n > m) return false;
+        for(char ch : s1){
+            fre1[ch - 'a']++;
+        }
+        for(int i = 0 ; i < n ; i++){
+            fre2[s2[i] - 'a']++;
+        }
+        if(check(fre1, fre2)) return true;
+        for(int i = n ; i < m ; i++){
+            fre2[s2[i] - 'a']++;
+            fre2[s2[i - n] - 'a']--;
+            if(check(fre1, fre2)) return true;
         }
         return false;
     }
