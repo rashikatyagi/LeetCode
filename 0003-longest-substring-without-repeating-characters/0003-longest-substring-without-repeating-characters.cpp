a77005b5b90c27b9ea03f0380d887a60c58e5e03
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-07 22:05:30 +0530

    Time: 13 ms (59.4%), Space: 12 MB (66.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,14 +7,13 @@ public:
         int index = 0;
         int j = 0;
         while(j < n){
-            if(mp.find(s[j]) == mp.end()){
+            if(mp.find(s[j]) == mp.end() || mp[s[j]] < index){
                 maxLength = max(maxLength, j - index + 1);
                 mp[s[j]] = j;
             }
             else{
-                j = mp[s[j]];
-                index = j + 1;
-                mp.clear();
+                index = mp[s[j]] + 1;
+                continue;
             }
             j++;
         }
