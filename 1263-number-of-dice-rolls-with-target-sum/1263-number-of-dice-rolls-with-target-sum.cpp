15a60fdf05065a38794b05e8ca5f51ce136cf373
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-08 22:49:21 +0530

    Time: 34 ms (10.9%), Space: 10 MB (31.84%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
@@ -42,8 +42,25 @@ public:
         }
         return dp[n][target] = ans;
     }    
+    int solveTab(int n, int k, int target){
+        vector<vector<int> > dp(n + 1, vector<int>(target + 1, 0));
+        dp[0][0] = 1;
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j<= target ; j++){
+                int ans = 0;
+                for(int c = 1 ; c <= k ; c++){
+                    if(j - c >= 0){
+                        ans = ((ans % MOD) + (dp[i - 1][j - c]) % MOD) % MOD;
+                    }
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[n][target];
+    }    
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int> > dp(n + 1, vector<int>(target + 1, -1));
-        return solveMem(n, k, target, dp);
+        //vector<vector<int> > dp(n + 1, vector<int>(target + 1, -1));
+        return solveTab(n, k, target);
     }
 };
