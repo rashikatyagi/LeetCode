0c051887e3f4c931b6fc637a1dcbf80a54ad06c1
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-08 21:43:21 +0530

    Time: 5 ms (30.57%), Space: 10.6 MB (76.85%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -10,20 +10,18 @@
  */
 class Solution {
 public:
-    ListNode* solve(ListNode* temp1, ListNode* temp2){
-        //base case 
-        if(temp1 == NULL) return NULL;
-        if(temp2 == NULL) return temp1;
-        ListNode* temp = temp2->next;
-        temp2->next = temp1;
-        if(temp == NULL)
-            temp1->next = solve(NULL, NULL);
-        else
-            temp1->next = solve(temp, temp->next);
-        return temp2;
-    }
     ListNode* swapPairs(ListNode* head) {
-        if(head == NULL || head->next == NULL) return head;
-        return solve(head, head->next);
+        if(head == NULL) return NULL;
+        if(head->next == NULL) return head;
+        ListNode* temp = head->next->next;
+        head->next->next = head;
+        ListNode* newhead = head->next;
+        if(temp == NULL){
+            head->next = swapPairs(NULL);
+        }
+        else{
+            head->next = swapPairs(temp);
+        }
+        return newhead;
     }
 };
