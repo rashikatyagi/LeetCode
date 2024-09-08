8297372ebf068a3f74ca6bfc4fd16b0efa8118e2
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-08 16:06:39 +0530

    Time: 33 ms (19.72%), Space: 94.8 MB (70.26%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,32 +3,42 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
+        int total = n + m;
+        int index1 = (total / 2) - 1;
+        int index2 = (total / 2);
+        int ele1 = -1, ele2 = -1;
         int i = 0, j = 0;
-        vector<int> arr;
+        int count = 0;
         while(i < n && j < m){
             if(nums1[i] < nums2[j]){
-                arr.push_back(nums1[i++]);
+                if(count == index1) ele1 = nums1[i];
+                if(count == index2) ele2 = nums1[i];
+                i++;
+                count++;
             }
             else{
-                arr.push_back(nums2[j++]);
+                if(count == index1) ele1 = nums2[j];
+                if(count == index2) ele2 = nums2[j];
+                j++;
+                count++;
             }
-        }
+            if(ele1 != -1 && ele2 != -1) break;
+        } 
         while(i < n){
-            arr.push_back(nums1[i++]);
+            if(count == index1) ele1 = nums1[i];
+            if(count == index2) ele2 = nums1[i];
+            i++;
+            count++;
+            if(ele1 != -1 && ele2 != -1) break;
         }
         while(j < m){
-            arr.push_back(nums2[j++]);
+            if(count == index1) ele1 = nums2[j];
+            if(count == index2) ele2 = nums2[j];
+            j++;
+            count++;
+            if(ele1 != -1 && ele2 != -1) break;
         }
-        int final_size = arr.size();
-        double ans;
-        if(final_size & 1){
-            ans = arr[final_size / 2];
-        }
-        else{
-            int index1 = (final_size - 1) / 2;
-            int index2 = final_size / 2;
-            ans = (arr[index1] + arr[index2]) / (double)2;
-        }
-        return ans;
+        if(total & 1) return ele2;
+        return (double)(ele1 + ele2) / (double)2;
     }
 };
