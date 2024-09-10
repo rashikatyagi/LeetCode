4e3259ab77d10dd12d0e40ffb15ac67ecf456ad6
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-10 13:58:41 +0530

    Time: 209 ms (5.04%), Space: 71.3 MB (32.32%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -3,21 +3,22 @@ public:
     int maxOperations(vector<int>& nums, int target) {
         int n = nums.size();
         if(n == 1) return 0;
-	    sort(nums.begin(), nums.end());
-        int start = 0; 
-        int end = n - 1;
+	    unordered_map<int, int> mp;
+	    for(int i = 0 ; i < n ; i++){
+	        mp[nums[i]]++;
+        }
+        for(auto it : mp){
+            cout << it.first << " : " << it.second << endl;
+        }
         int operations = 0;
-        while(start < end){
-            if(nums[start] + nums[end] == target){
-                operations++;
-                start++;
-                end--;
-            }
-            else if(nums[start] + nums[end] < target){
-                start++;
-            }
-            else{
-                end--;
+        for(int i = 0 ; i < n ; i++){
+	        int element = target - nums[i];
+            if(mp.find(nums[i]) != mp.end() && mp[nums[i]] > 0){
+                mp[nums[i]]--;
+	            if(mp.find(element) != mp.end() && mp[element] > 0){
+		            operations++;
+		            mp[element]--;
+                }
             }
         }
         return operations;
