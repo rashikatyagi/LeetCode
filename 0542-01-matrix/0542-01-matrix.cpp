e59146e3b4844355584a91ee509eb5c1c2f24bd6
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-15 12:19:54 +0530

    Time: 72 ms (13.96%), Space: 37.6 MB (58.09%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -14,31 +14,41 @@ public:
                 }
             }
         }
+        int delrow[4] = {-1, 0, 1, 0};
+        int delcol[4] = {0, 1, 0, -1};
         while(!q.empty()){
             int frontRow = q.front().first.first;
             int frontCol = q.front().first.second;
             int dist = q.front().second;
             q.pop();
             answer[frontRow][frontCol] = dist;
-            //UP
-            if(frontRow - 1 >= 0 && !visited[frontRow - 1][frontCol]){
-                visited[frontRow - 1][frontCol] = 1;
-                q.push({{frontRow - 1, frontCol}, dist + 1});
-            }
-            //DOWN
-            if(frontRow + 1 < m && !visited[frontRow + 1][frontCol]){
-                visited[frontRow + 1][frontCol] = 1;
-                q.push({{frontRow + 1, frontCol}, dist + 1});
-            }
-            //LEFT
-            if(frontCol - 1 >= 0 && !visited[frontRow][frontCol - 1]){
-                visited[frontRow][frontCol - 1] = 1;
-                q.push({{frontRow, frontCol - 1}, dist + 1});
-            }
-            //RIGHT
-            if(frontCol + 1 < n && !visited[frontRow][frontCol + 1]){
-                visited[frontRow][frontCol + 1] = 1;
-                q.push({{frontRow, frontCol + 1}, dist + 1});
+            // //UP
+            // if(frontRow - 1 >= 0 && !visited[frontRow - 1][frontCol]){
+            //     visited[frontRow - 1][frontCol] = 1;
+            //     q.push({{frontRow - 1, frontCol}, dist + 1});
+            // }
+            // //DOWN
+            // if(frontRow + 1 < m && !visited[frontRow + 1][frontCol]){
+            //     visited[frontRow + 1][frontCol] = 1;
+            //     q.push({{frontRow + 1, frontCol}, dist + 1});
+            // }
+            // //LEFT
+            // if(frontCol - 1 >= 0 && !visited[frontRow][frontCol - 1]){
+            //     visited[frontRow][frontCol - 1] = 1;
+            //     q.push({{frontRow, frontCol - 1}, dist + 1});
+            // }
+            // //RIGHT
+            // if(frontCol + 1 < n && !visited[frontRow][frontCol + 1]){
+            //     visited[frontRow][frontCol + 1] = 1;
+            //     q.push({{frontRow, frontCol + 1}, dist + 1});
+            // }
+            for(int i = 0 ; i < 4 ; i++){
+                int newRow = frontRow + delrow[i];
+                int newCol = frontCol + delcol[i];
+                if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !visited[newRow][newCol]) {
+                    visited[newRow][newCol] = 1;
+                    q.push({{newRow, newCol}, dist + 1});
+                }
             }
         }
         return answer;
