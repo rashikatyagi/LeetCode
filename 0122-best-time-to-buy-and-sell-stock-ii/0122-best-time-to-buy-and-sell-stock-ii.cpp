2393168feaa90d4e198c4aaef155de4161f1b708
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-16 12:23:41 +0530

    Time: 12 ms (13.86%), Space: 18 MB (30.47%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -13,26 +13,48 @@ public:
     //     }
     //     return profit;
     // }
-    int solveMEM(int index, bool buy, vector<int>& prices, vector<vector<int> > &dp){
-        if(index == prices.size()){
-            return 0;
+    // int solveMEM(int index, bool buy, vector<int>& prices, vector<vector<int> > &dp){
+    //     if(index == prices.size()){
+    //         return 0;
+    //     }
+    //     if(dp[index][buy] != -1){
+    //         return dp[index][buy];
+    //     }
+    //     int profit;
+    //     if(buy){ //can buy
+    //         profit = max(-prices[index] + solveMEM(index + 1, 0, prices, dp), solveMEM(index + 1, 1, prices, dp));
+    //     }
+    //     else{ //can sell
+    //         profit = max(prices[index] + solveMEM(index + 1, 1, prices, dp), solveMEM(index + 1, 0, prices, dp));
+    //     }
+    //     return dp[index][buy] = profit;
+    // }
+    int solveTAB(vector<int>& prices, int n){
+        vector<vector<int> > dp(n, vector<int>(2, 0));
+        dp[n - 1][0] = prices[n - 1];
+
+        for(int index = n - 2 ; index >= 0 ; index--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+                int profit;
+                if(buy){ //can buy
+                    profit = max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
+                }
+                else{ //can sell
+                    profit = max(prices[index] + dp[index + 1][1], dp[index + 1][0]);
+                }
+
+                dp[index][buy] = profit;
+            }
         }
-        if(dp[index][buy] != -1){
-            return dp[index][buy];
-        }
-        int profit;
-        if(buy){ //can buy
-            profit = max(-prices[index] + solveMEM(index + 1, 0, prices, dp), solveMEM(index + 1, 1, prices, dp));
-        }
-        else{ //can sell
-            profit = max(prices[index] + solveMEM(index + 1, 1, prices, dp), solveMEM(index + 1, 0, prices, dp));
-        }
-        return dp[index][buy] = profit;
+        
+        return dp[0][1];
     }
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int> > dp(n, vector<int>(2, -1));
+        // vector<vector<int> > dp(n, vector<int>(2, -1));
         // return solveRE(0, 1, prices);
-        return solveMEM(0, 1, prices, dp);
+        // return solveMEM(0, 1, prices, dp);
+        return solveTAB(prices, n);
     }
 };
