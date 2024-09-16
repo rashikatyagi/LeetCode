56b1e4f028bbfaa2a276623586c6fd2c95a535bc
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-09-16 10:27:45 +0530

    Time: 88 ms (44.93%), Space: 96.3 MB (26.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,8 @@ public:
         int profit = 0;
         int minimum = prices[0];
         for(int i = 1 ; i < prices.size() ; i++){
-            //profit = max(profit, prices[i] - minimum);
-            if(prices[i] - minimum > profit) profit = prices[i] - minimum;
-            //minimum = min(minimum, prices[i]);
+            int tempProfit = prices[i] - minimum;
+            if(tempProfit > profit) profit = tempProfit;
             if(prices[i] < minimum) minimum = prices[i];
         }
         return profit;
