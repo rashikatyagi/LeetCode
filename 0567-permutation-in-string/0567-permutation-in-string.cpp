5684dd4e9f0b3c5dab86fb6a92d070c7921ce73a
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-10-05 17:29:33 +0530

    Time: 4 ms (81.5%), Space: 8.8 MB (50.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,11 +3,18 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.length();
         int m = s2.length();
-        sort(s1.begin(), s1.end());
-        for(int i = 0 ; i <= m - n ; i++){
-            string temp = s2.substr(i, n);
-            sort(temp.begin(), temp.end());
-            if(temp == s1) return true;
+        if(n > m) return false;
+        vector<int> frequency1(26, 0);
+        vector<int> frequency2(26, 0);
+        for(int i = 0 ; i < n ; i++){
+            frequency1[s1[i] - 'a']++;
+            frequency2[s2[i] - 'a']++;
+        }
+        if(frequency1 == frequency2) return true;
+        for(int i = n ; i < m ; i++){
+            frequency2[s2[i] - 'a']++;
+            frequency2[s2[i - n] - 'a']--;
+            if(frequency1 == frequency2) return true;
         }
         return false;
     }
