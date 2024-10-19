ffcf19108ece8c5d8c3c8ad0542cbd744266719a
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-10-19 18:39:42 +0530

    Time: 29 ms (43.52%), Space: 21.1 MB (54.1%) - LeetHub

---
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        string temp = "";
+        if(k == 1) return '0';
+        while(temp.length() * 2 + 1 <= k && (n--) >= 0){
+            if(temp == "") temp = "0";
+            else{
+                temp.push_back('1');
+                for(int i = temp.length() - 2 ; i >= 0 ; i--){
+                    if(temp[i] == '1') temp.push_back('0');
+                    else temp.push_back('1');
+                }
+            }
+        }
+        int len = temp.length();
+        
+        if(k == len + 1) return '1';
+        else{
+            char val = temp[(2 * len + 1) - k];
+            if(val == '1') return '0';
+            else return '1';
+        }
+    }
+};
