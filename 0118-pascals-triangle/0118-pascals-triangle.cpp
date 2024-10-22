747ca7ff61f38bff53345901231c10aa9b7227de
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-10-23 05:06:24 +0530

    Time: 2 ms (40.84%), Space: 8.1 MB (73.98%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> triangle;
-        
-        if(numRows >= 1) triangle.push_back({1});
-        if(numRows >= 2) triangle.push_back({1, 1});
-        int currRow = 3;
-        while(numRows-- > 2){
-            vector<int> temp(currRow);
-            for(int i = 0 ; i < currRow ; i++){
-                if(i == 0 || i == currRow - 1){
-                    temp[i] = 1;
+        for(int rows = 1 ; rows <= numRows ; rows++){
+            vector<int> temp(rows);
+            for(int j = 0 ; j < rows ; j++){
+                if(j == 0 || j == rows - 1){
+                    temp[j] = 1;
                 }
                 else{
-                    temp[i] = triangle[currRow - 2][i] + triangle[currRow - 2][i - 1];
+                    temp[j] = triangle[rows - 2][j] + triangle[rows - 2][j - 1];
                 }
             }
             triangle.push_back(temp);
-            currRow++;
         }
         return triangle;
     }
