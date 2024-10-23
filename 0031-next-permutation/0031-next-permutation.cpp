d3e0f8d24576289f21506bebbabaee98092faae1
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-10-23 06:24:15 +0530

    Time: 0 ms (100%), Space: 14.8 MB (49%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        //step-1 get the dip : where first element is smaller than the next
+        int index = -1;
+        for(int i = n - 1 ; i > 0 ; i--){
+            if(nums[i] > nums[i - 1]){
+                index = i - 1;
+                break;
+            }
+        }
+        //if it is not found that means we are already at the end of the permutation we just need to reverse it
+        if(index == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        //step 2 get the smallest element greater than element at index
+        for(int i = n - 1 ; i > index ; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        //step 3 reverse the remaining array
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
