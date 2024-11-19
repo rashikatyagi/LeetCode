a7d00e586442922888f94d281176a2c39ebe6d7e
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-19 08:34:36 +0530

    Time: 0 ms (100%), Space: 17.2 MB (52.45%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
-    void bfs(int row, int col, int pixel, vector<vector<int>>& image, vector<vector<bool>>& visited, int color) {
+    void bfs(vector<vector<int>>& image, int sr, int sc, int color, int initialcolor){
+        int m = image.size();
+        int n = image[0].size();
         queue<pair<int, int>> q;
-        //color and visit the starting node
-        image[row][col] = color;
-        visited[row][col] = 1;
-        q.push({row, col});
+        q.push({sr, sc});
         while(!q.empty()){
-            int frontRow = q.front().first;
-            int frontCol = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
-            if(frontRow - 1 >= 0 && image[frontRow - 1][frontCol] == pixel && !visited[frontRow - 1][frontCol]){
-                visited[frontRow - 1][frontCol] = 1;
-                image[frontRow - 1][frontCol] = color;
-                q.push({frontRow - 1, frontCol});
-            }
-            if(frontRow + 1 < image.size() && image[frontRow + 1][frontCol] == pixel && !visited[frontRow + 1][frontCol]){
-                visited[frontRow + 1][frontCol] = 1;
-                image[frontRow + 1][frontCol] = color;
-                q.push({frontRow + 1, frontCol});
-            }
-            if(frontCol - 1 >= 0 && image[frontRow][frontCol - 1] == pixel && !visited[frontRow][frontCol - 1]){
-                visited[frontRow][frontCol - 1] = 1;
-                image[frontRow][frontCol - 1] = color;
-                q.push({frontRow, frontCol - 1});
-            }
-            if(frontCol + 1 < image[0].size() && image[frontRow][frontCol + 1] == pixel && !visited[frontRow][frontCol + 1]){
-                visited[frontRow][frontCol + 1] = 1;
-                image[frontRow][frontCol + 1] = color;
-                q.push({frontRow, frontCol + 1});
-            }
+            image[row][col] = color;
+            if(row - 1 >= 0 && image[row - 1][col] == initialcolor) q.push({row - 1, col});
+            if(row + 1 < m && image[row + 1][col] == initialcolor) q.push({row + 1, col});
+            if(col - 1 >= 0 && image[row][col - 1] == initialcolor) q.push({row, col - 1});
+            if(col + 1 < n && image[row][col + 1] == initialcolor) q.push({row, col + 1});
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        if(image[sr][sc] == color) return image;
-        vector<vector<bool> > visited(n, vector<bool>(m, false));
-    
-        //apply bfs
-        bfs(sr, sc, image[sr][sc], image, visited, color);
+        int initialcolor = image[sr][sc];
+        if(initialcolor == color) return image;
+        bfs(image, sr, sc, color, initialcolor);
         return image;
     }
 };
