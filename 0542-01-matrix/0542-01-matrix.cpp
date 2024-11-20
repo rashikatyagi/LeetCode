9384941d55ab7641b9fbbcfc6227f875d97d91bd
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-20 10:32:44 +0530

    Time: 24 ms (45.87%), Space: 37.6 MB (51.63%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,35 +3,40 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        queue<pair<pair<int, int>, int> > q;
-        vector<vector<bool>> visited(m, vector<bool>(n, 0));
-        vector<vector<int>> answer(m, vector<int>(n, 0));
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(mat[i][j] == 0){
-                    q.push({{i, j}, 0});
-                    visited[i][j] = 1;
+        vector<vector<bool>> visited(m, vector<bool> (n, 0));
+        vector<vector<int>> distance(m, vector<int>(n, 0));
+        queue<pair<int, pair<int, int>>> q;
+        for(int row = 0 ; row < m ; row++){
+            for(int col = 0 ; col < n ; col++){
+                if(mat[row][col] == 0){
+                    q.push({0, {row, col}});
+                    visited[row][col] = 1;
                 }
             }
         }
-        int delrow[4] = {-1, 0, 1, 0};
-        int delcol[4] = {0, 1, 0, -1};
         while(!q.empty()){
-            int frontRow = q.front().first.first;
-            int frontCol = q.front().first.second;
-            int dist = q.front().second;
+            int dist = q.front().first;
+            int currrow = q.front().second.first;
+            int currcol = q.front().second.second;
             q.pop();
-            answer[frontRow][frontCol] = dist;
-            
-            for(int i = 0 ; i < 4 ; i++){
-                int newRow = frontRow + delrow[i];
-                int newCol = frontCol + delcol[i];
-                if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !visited[newRow][newCol]) {
-                    visited[newRow][newCol] = 1;
-                    q.push({{newRow, newCol}, dist + 1});
-                }
+            distance[currrow][currcol] = dist;
+            if(currrow - 1 >= 0 && visited[currrow - 1][currcol] == 0){
+                q.push({dist + 1, {currrow - 1, currcol}});
+                visited[currrow - 1][currcol] = 1;
+            }
+            if(currrow + 1 < m && visited[currrow + 1][currcol] == 0){
+                q.push({dist + 1, {currrow + 1, currcol}});
+                visited[currrow + 1][currcol] = 1;
+            }
+            if(currcol - 1 >= 0 && visited[currrow][currcol - 1] == 0){
+                q.push({dist + 1, {currrow, currcol - 1}});
+                visited[currrow][currcol - 1] = 1;
+            }
+            if(currcol + 1 < n && visited[currrow][currcol + 1] == 0){
+                q.push({dist + 1, {currrow, currcol + 1}});
+                visited[currrow][currcol + 1] = 1;
             }
         }
-        return answer;
+        return distance;
     }
 };
