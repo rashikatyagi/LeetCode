1c5e249623aa6e2aacf2a767c0688d772f2199a2
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-20 09:48:20 +0530

    Time: 0 ms (100%), Space: 15 MB (18.16%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<char>>& board, vector<vector<bool>> &visited){
+    void dfs(int row, int col, int m, int n, vector<vector<char>>& board, vector<vector<bool>>& visited){
         visited[row][col] = 1;
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
-        int m = board.size();
-        int n = board[0].size();
-        for(int i = 0 ; i < 4 ; i++){
-            int newRow = row + delrow[i];
-            int newCol = col + delcol[i];
-            if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && board[newRow][newCol] == 'O' && !visited[newRow][newCol]){
-                dfs(newRow, newCol, board, visited);
-            }
+        if(row - 1 >= 0 && board[row - 1][col] == 'O' && visited[row - 1][col] == 0){
+            dfs(row - 1, col, m, n, board, visited);
+        }
+        if(row + 1 < m && board[row + 1][col] == 'O' && visited[row + 1][col] == 0){
+            dfs(row + 1, col, m, n, board, visited);
+        }
+        if(col - 1 >= 0 && board[row][col - 1] == 'O' && visited[row][col - 1] == 0){
+            dfs(row, col - 1, m, n, board, visited);
+        }
+        if(col + 1 < n && board[row][col + 1] == 'O' && visited[row][col + 1] == 0){
+            dfs(row, col + 1, m, n, board, visited);
         }
     }
     void solve(vector<vector<char>>& board) {
         int m = board.size();
         int n = board[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
-        //checking first and last row
-        for(int i = 0 ; i < n ; i++){
-            //first row
-            if(board[0][i] == 'O'){
-                dfs(0, i, board, visited);
+        vector<vector<bool>> visited(m, vector<bool>(n, 0));
+        for(int row = 0 ; row < m ; row++){
+            if(board[row][0] == 'O'){
+                dfs(row, 0, m, n, board, visited);
             }
-            //last row
-            if(board[m - 1][i] == 'O'){
-                dfs(m - 1, i, board, visited);
+            if(board[row][n - 1] == 'O'){
+                dfs(row, n - 1, m, n, board, visited);
             }
         }
-
-        //checking first and last column
-        for(int i = 0 ; i < m ; i++){
-            //first column
-            if(board[i][0] == 'O'){
-                dfs(i, 0, board, visited);
+        for(int col = 0 ; col < n ; col++){
+            if(board[0][col] == 'O'){
+                dfs(0, col, m, n, board, visited);
             }
-            //last column
-            if(board[i][n - 1] == 'O'){
-                dfs(i, n - 1, board, visited);
+            if(board[m - 1][col] == 'O'){
+                dfs(m - 1, col, m, n, board, visited);
             }
         }
-
-        //checking 'O' that are not marked
+        //convert all the Os that are not visited to X
         for(int row = 1 ; row < m - 1 ; row++){
             for(int col = 1 ; col < n - 1 ; col++){
-                if(board[row][col] == 'O' && !visited[row][col]){
+                if(board[row][col] == 'O' && visited[row][col] == 0){
                     board[row][col] = 'X';
                 }
             }
