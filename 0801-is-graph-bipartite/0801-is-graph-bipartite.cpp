1f51e52f8f8716c054fb08011bdf7882d993d304
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-21 17:01:47 +0530

    Time: 0 ms (100%), Space: 16.7 MB (34.33%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    // bool bfs(int source, vector<vector<int>>& graph, vector<int> &color){
-    //     queue<int> q;
-    //     q.push(source);
-    //     color[source] = 1;
-    //     while(!q.empty()){
-    //         int currNode = q.front();
-    //         q.pop();
-    //         for(auto it : graph[currNode]){
-    //             if(color[it] == -1){
-    //                 color[it] = !color[currNode];
-    //                 q.push(it);
-    //             }
-    //             else if(color[it] == color[currNode]){
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
-    bool dfs(int source, vector<vector<int>>& graph, vector<int> &color, int currColor){
-        color[source] = currColor;
-        for(auto it : graph[source]){
-            if(color[it] == -1){
-                if(dfs(it, graph, color, !currColor) == false) {
+    bool bfs(int source, vector<vector<int>>& graph, vector<int> &color){
+        color[source] = 0;
+        queue<int> q;
+        q.push(source);
+        while(!q.empty()){
+            int currnode = q.front();
+            q.pop();
+            for(auto it : graph[currnode]){
+                if(color[it] == -1){
+                    color[it] = !color[currnode];
+                    q.push(it);    
+                }
+                else if(color[it] == color[currnode]){
                     return false;
                 }
-            }
-            else if(color[it] == color[source]){
-                return false;
             }
         }
         return true;
@@ -36,19 +22,9 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> color(n, -1);
-        //for disconnected components
-        // for(int i = 0 ; i < n ; i++){
-        //     if(color[i] == -1){
-        //         if(bfs(i, graph, color) == false){
-        //             return false;
-        //         }
-        //     }
-        // }
         for(int i = 0 ; i < n ; i++){
             if(color[i] == -1){
-                if(dfs(i, graph, color, 0) == false){
-                    return false;
-                }
+                if(!bfs(i, graph, color)) return false;
             }
         }
         return true;
