eee5f698d9ae6cf3ea341816462550d954c62ec3
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-21 11:25:40 +0530

    Time: 3 ms (49.23%), Space: 58.3 MB (88.37%) - LeetHub

--- 1254-deepest-leaves-sum/1254-deepest-leaves-sum.cpp
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.cpp
@@ -11,24 +11,25 @@
  */
 class Solution {
 public:
-    int height(TreeNode* &root){
-        while(root == NULL) return 0;
-        return max(1 + height(root->left), 1 + height(root->right));
-    }
-    void getSum(TreeNode* &root, int level, int &height, int &sum){
-        if(root == NULL) return; 
+    void getSum(TreeNode* root, int level, int &maxLevel, int &sum){
+        if(root == NULL) return;
         if(root->left == NULL && root->right == NULL){
-            //leaf node
-            if(level == height) sum += root->val;
+            if(level > maxLevel){
+                maxLevel = level;
+                sum = root->val;
+            }
+            else if(level == maxLevel){
+                sum += root->val;
+            }
             return;
-        }
-        getSum(root->left, level + 1, height, sum);
-        getSum(root->right, level + 1, height, sum);
+        } 
+        getSum(root->left, level + 1, maxLevel, sum);
+        getSum(root->right, level + 1, maxLevel, sum);
     }
     int deepestLeavesSum(TreeNode* root) {
-        int depth = height(root);
-        int answer = 0;
-        getSum(root, 1, depth, answer);
+        int answer = 0 ;
+        int maxlevel = 1;
+        getSum(root, 1, maxlevel, answer);
         return answer;
     }
 };
