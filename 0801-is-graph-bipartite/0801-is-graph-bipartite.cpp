754440fb8bae46426cd52b064a74d08a88fc03f4
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-21 17:06:08 +0530

    Time: 0 ms (100%), Space: 16.4 MB (78.21%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    bool bfs(int source, vector<vector<int>>& graph, vector<int> &color){
-        color[source] = 0;
-        queue<int> q;
-        q.push(source);
-        while(!q.empty()){
-            int currnode = q.front();
-            q.pop();
-            for(auto it : graph[currnode]){
-                if(color[it] == -1){
-                    color[it] = !color[currnode];
-                    q.push(it);    
-                }
-                else if(color[it] == color[currnode]){
-                    return false;
-                }
+    // bool bfs(int source, vector<vector<int>>& graph, vector<int> &color){
+    //     color[source] = 0;
+    //     queue<int> q;
+    //     q.push(source);
+    //     while(!q.empty()){
+    //         int currnode = q.front();
+    //         q.pop();
+    //         for(auto it : graph[currnode]){
+    //             if(color[it] == -1){
+    //                 color[it] = !color[currnode];
+    //                 q.push(it);    
+    //             }
+    //             else if(color[it] == color[currnode]){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool dfs(int source, int c, vector<vector<int>>& graph, vector<int> &color){
+        color[source] = c;
+        for(auto it : graph[source]){
+            if(color[it] == -1){
+                if(!dfs(it, !c, graph, color)) return false;
             }
+            else if(color[it] == color[source]) return false;
         }
         return true;
     }
@@ -24,7 +34,8 @@ public:
         vector<int> color(n, -1);
         for(int i = 0 ; i < n ; i++){
             if(color[i] == -1){
-                if(!bfs(i, graph, color)) return false;
+                //if(!bfs(i, graph, color)) return false;
+                if(!dfs(i, 0, graph, color)) return false;
             }
         }
         return true;
