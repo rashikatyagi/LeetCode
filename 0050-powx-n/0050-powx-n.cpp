33a3502f66688bebd27f17e02a007e91d8f12c3f
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-23 16:00:02 +0530

    Time: 0 ms (100%), Space: 8.3 MB (71.34%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
+    double power(double x, int n){
+        if(n == 0) return 1;
+        if(n & 1){
+            return x * power(x * x, n / 2);
+        }
+        else return power(x * x, n / 2);
+    }
     double myPow(double x, int n) {
-        if(n < 0){
-            x = 1 / x;
+        if(x == 1 || n == 0) return 1;
+        if(x == -1){
+            if(n & 1) return -1;
+            return 1;
         }
-        long num = labs(n);
-        if(x == 1 || num == 0) return 1;
-        if(x == -1 && !(num & 1)) return 1;
-        if(x == -1 && (num & 1)) return -1;
-        double ans = 1;
-        while(num){
-            if(num & 1){
-                ans *= x;
-            }
-            x *= x;
-            num >>= 1;
-        }
-        return ans;
+        if(n < 0) x = 1 / x;
+        return power(x, n);
     }
 };
