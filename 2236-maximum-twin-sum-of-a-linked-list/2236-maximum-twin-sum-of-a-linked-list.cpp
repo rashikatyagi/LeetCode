582cea955bd9496d9c48bb77d2daabf94df41888
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-11-24 15:51:51 +0530

    Time: 1 ms (78.56%), Space: 123.3 MB (56.07%) - LeetHub

---
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reverseLL(ListNode* &head){
+        if(head == NULL || head->next == NULL){
+            return;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* nextnode = head->next;
+        while(curr != NULL){
+            curr->next = prev;
+            prev = curr;
+            curr = nextnode;
+            if(nextnode != NULL) nextnode = nextnode->next;
+        }
+        head = prev;
+    }
+    int pairSum(ListNode* head) {
+        if(head == NULL || head->next == NULL) return 0;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL){
+            slow = slow->next;
+            fast = fast->next;
+            if(fast != NULL) fast = fast->next;
+        }
+        ListNode* new_head = slow;
+        reverseLL(new_head);
+        int currsum = 0;
+        int maxsum = INT_MIN;
+        while(new_head != NULL){
+            currsum = head->val + new_head->val;
+            maxsum = max(maxsum, currsum);
+            head = head->next;
+            new_head = new_head->next;
+        }
+        return maxsum;
+    }
+};
