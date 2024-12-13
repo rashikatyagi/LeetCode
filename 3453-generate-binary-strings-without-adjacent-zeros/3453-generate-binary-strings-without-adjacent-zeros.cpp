5623a98f3a27191b194f4dcb040537d5e6c8c498
Rashika Tyagi <114401627+rashikatyagi@users.noreply.github.com>
2024-12-14 00:34:25 +0530

    Time: 7 ms (92.85%), Space: 20.9 MB (72.22%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    void generate(int n, string currstring, vector<string> &ans){
+    void generate(int n, string &currstring, vector<string> &ans){
         int len = currstring.length();
         if(len == n){
             ans.push_back(currstring);
             return;
         }
         if(len == 0 || currstring[len - 1] != '0'){
-            generate(n, currstring + '0', ans);
-            generate(n, currstring + '1', ans);
+            currstring.push_back('0');
+            generate(n, currstring, ans);
+            currstring.pop_back();
+            currstring.push_back('1');
+            generate(n, currstring, ans);
+            currstring.pop_back();
         }
-        else generate(n, currstring + '1', ans);
+        else{
+            currstring.push_back('1');
+            generate(n, currstring, ans);
+            currstring.pop_back();
+
+        }
     }
     vector<string> validStrings(int n) {
         vector<string> answer;
-        generate(n, "", answer);
+        string temp = "";
+        generate(n, temp, answer);
         return answer;
     }
 };
